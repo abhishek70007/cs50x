31b6d0f1fddc9ba2d4f1ebc6acfb044d9da19996
abhishek70007 <132220958+abhishek70007@users.noreply.github.com>
2023-05-25 20:08:47 +0530

    Delete Week 7 - SQL directory

--- Week 7 - SQL/Lab7/Songs/2.sql
+++
@@ -1,8 +0,0 @@
-/*
-Write a SQL query to list the names of all songs in increasing order of tempo.
-Your query should output a table with a single column for the name of each song.
-*/
-
-SELECT name 
-FROM songs
-ORDER BY tempo;
